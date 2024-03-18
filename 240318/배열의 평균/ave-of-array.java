71c1efcccd4b9cd07b47618c7bb6e2e05a223f09
Minji <120402129+meanzi3@users.noreply.github.com>
2024-03-18 23:43:18 +0900

    Title: 배열의 평균, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/배열의 평균/ave-of-array.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[2][4];
+
+        for(int i = 0; i < 2 ; i++){
+            for(int j = 0; j < 4; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        } // 배열에 담기
+
+        // 1. 가로 평균
+        for(int i = 0; i < 2; i++){
+            int sum = 0;
+            for(int j = 0; j < 4; j++){
+                sum += arr[i][j];
+            }
+            double avg = sum / 4;
+            System.out.printf("%.1f ", avg);
+        }
+        System.out.println();
+
+        // 2. 세로 평균
+        for(int i = 0; i < 4; i++){
+            int sum = 0;
+            for(int j = 0; j < 2; j++){
+                sum += arr[j][i];
+            }
+            double avg = sum / 2;
+            System.out.printf("%.1f ", avg);
+        }
+        System.out.println();
+
+        // 3. 전체 평균
+        int sum = 0;
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 4; j++){
+                sum += arr[i][j];
+            }
+        }
+        double avg = sum / 8;
+        System.out.printf("%.1f ", avg);
+    }
+}
