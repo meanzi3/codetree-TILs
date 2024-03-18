863b5c6467e34e059663ac63561de5212310abe3
Minji <120402129+meanzi3@users.noreply.github.com>
2024-03-18 23:28:59 +0900

    Title: 대문자로 바꾸기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(sc.next().toUpperCase()+" ");
+            }
+            System.out.println();
+        }
+    }
+}
